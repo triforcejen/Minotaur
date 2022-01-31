ab8385bdbb8810a7c9b6ac77fe1bd529ce727cef
triforce <triforcejen@gmail.com>
2022-01-31 16:26:20 +0000

    Add basic versions of functions to game loop.

--- include/game_loop.h
+++ include/game_loop.h
@@ -3,6 +3,20 @@
 
 #include "bool.h"
 
+void Print_Menu(void);
+
+void Fight(void);
+
+void Rest(void);
+
+void Sacrifice(void);
+
+void Pray(void);
+
+void Stats(void);
+
+void Process_Input(char);
+
 bool Game_Loop(void);
 
 #endif
--- source/game_loop.c
+++ source/game_loop.c
@@ -5,7 +5,73 @@ void Debug(void){
 	printf("Game running... \n");
 }
 
+void Print_Menu(void){
+	printf(" Q: Fight.\t W: Rest.\t E: Sacrifice.\t R: Pray.\t T: Stats.\n");
+}
+
+void Fight(void){}
+void Rest(void){}
+void Sacrifice(void){}
+void Pray(){}
+
+void Stats(){
+	printf("\t LEVEL: ");
+	printf("\t HP: ");
+	printf("\t ATTACK:");
+	printf("\t DEFENCE: ");
+}
+
+void Process_Input(char input){
+	
+	switch(input){
+		case 'q':
+		case 'Q':
+			Fight();
+			break;
+		case 'w':
+		case 'W':
+			Rest();
+			break;
+		case 'e':
+		case 'E':
+			Sacrifice();
+			break;
+		case 'r':
+		case 'R':
+			Pray();
+			break;
+		case 't':
+		case 'T':
+			Stats();
+			break;
+		default:
+			printf("Invalid input. \n");
+			break;
+	} 
+}
+
 bool Game_Loop(void){
-	Debug();
+	//Debug();
+
+	//Set menu loop variable.
+
+	//Start menu loop.
+
+		//Print menu options.
+		Print_Menu();
+		
+		//Print status.
+		Stats();
+		printf("\n");
+	
+		//Read player input.
+		char player_Input = getchar();
+		
+		//Process player input.
+		Process_Input(player_Input);
+
+		//Update game status.
+
+	//End menu loop.
 	return false;
 }
